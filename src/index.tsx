252b1699e83bdbd6e44f68bcafab64f23d9f719b
Mahira Noor <maheera.noor08@gmail.com>
2026-02-08 12:26:51 +0500

    Fix calculator import path for Vercel

--- src/index.tsx
+++ src/index.tsx
@@ -5,6 +5,8 @@ import { Toaster } from 'react-hot-toast';
 import './index.css';
 import App from './App';
 import { store } from './store';
+import Calculator from "./features/calculator/calculator";
+
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
